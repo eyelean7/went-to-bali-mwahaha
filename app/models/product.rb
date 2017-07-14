fab23ba27ebdaca9025a07a7b5147efd7ecc5270
Ilene Gorski <ilene.gorski@gmail.com>
2017-07-14 15:41:53 -0700

    allow admin to set float prices and fix spacing of product divs again

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :order_items
-  has_attached_file :image, styles: { medium: "300x300>"}, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "200x200>"}, default_url: "/images/:style/missing.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
